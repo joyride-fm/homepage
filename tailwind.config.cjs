3f2726f82e73fdd5c8bef9e9e9d94fc4b2dd1b5e
Mike Solomon <mike120982@gmail.com>
2022-11-02 13:22:40 +0200

    Small changes to assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,15 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,purs}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'mansion': "url('/assets/mansion.webp')",
-        'cat': "url('/assets/cat.webp')",
+        mansion: "url('/assets/mansion.webp')",
+        cat: "url('/assets/cat.webp')",
       },
-    },  },
-  plugins: [
-    require('flowbite/plugin'),
-    require('@tailwindcss/typography'),
-]
-}
+    },
+  },
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/typography")],
+};
